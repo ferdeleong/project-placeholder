aa7696546e9d2484e26c4122029da8d1d0612e96
dmosc <osdavrm@gmail.com>
2023-03-27 21:51:27 -0500

    tmp: hello sir
    Signed-off-by: dmosc <osdavrm@gmail.com>

--- app/scripts/redeployw5.sh
+++ app/scripts/redeployw5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd repos/project-placeholder;
+cd ~/repos/project-placeholder;
 
 # make sure the git repo inside VPS has lastest changes
-git stash && git switch main && git fetch --all && git reset --hard origin/main;
+git stash && git switch main && git fetch --all && git reset --hard origin/main
 
 # (Let's first spin containers down to prevent out of memory issues on our VPS instances when building in the next step)
 docker compose -f docker-compose.prod.yml down
